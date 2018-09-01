f308073788eec5b3f7a8b59367cd463d9365955a
rafael <rafaelcoaraujo@gmail.com>
2018-08-31 21:09:29 -0300

    Fixed find vote method.

--- repository/vote.go
+++ repository/vote.go
@@ -23,22 +23,11 @@ func newVoteRepository() *VoteRepository {
 	return &voteRepository
 }
 
-func (v *VoteRepository) InsertVote(vote domain.Vote) {
-	fmt.Println("[MONGODB] Inserting value: ", vote)
-
-	err := v.MongoDB.GetCollection(voteDatabase, voteCollectionName).Insert(vote)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("[MONGODB] Value inserted: ", vote)
-}
-
-func (v *VoteRepository) GetVote(groupId string) {
-	fmt.Println("[MONGODB] Getting vote for groupId:", groupId)
+func (v *VoteRepository) GetVoteById(id string) {
+	fmt.Println("[MONGODB] Getting vote by id:", id)
 
 	result := domain.Vote{}
-	err := v.MongoDB.GetCollection(voteDatabase, voteCollectionName).Find(bson.M{"name": groupId}).One(&result)
+	err := v.MongoDB.GetCollection(voteDatabase, voteCollectionName).Find(bson.M{"id": id}).One(&result)
 	if err != nil {
 		log.Fatal(err)
 	}
